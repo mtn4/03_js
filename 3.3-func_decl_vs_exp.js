2ee5c744e29a5a062655e51e7e7c7815a2cd88ab
‏‏‏‏Matan‏ ‏‏‏‏ ‏‏ ‏ <drms12@gmail.com>
2022-04-06 11:09:51 +0300

    3.3-func_decl_vs_exp

---
+++ 3.3-func_decl_vs_exp.js
@@ -0,0 +1,14 @@
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+const percentageOfWorld3 = (population) => {
+  return (population / 7900) * 100;
+};
+const percentageOfWorld4 = (population) => (population / 7900) * 100;
+console.log(percentageOfWorld1(1441));
+console.log(percentageOfWorld2(1441));
+console.log(percentageOfWorld3(1441));
+console.log(percentageOfWorld4(1441));
