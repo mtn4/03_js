10aab2cac38684cad72839e4b7fdd9908dd4d5d9
‏‏‏‏Matan‏ ‏‏‏‏ ‏‏ ‏ <drms12@gmail.com>
2022-04-06 14:58:54 +0300

    4.4 - boom

---
+++ 4.4 - boom.js
@@ -0,0 +1,15 @@
+function boom(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 7 == 0) {
+      let num = i.toString();
+      if (num.includes(7)) {
+        console.log("BOOM-BOOM");
+      } else {
+        console.log("BOOM");
+      }
+    } else {
+      console.log(i);
+    }
+  }
+}
+boom(18);
