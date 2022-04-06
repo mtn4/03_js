6ec6fa4ada97ca0a9e9cd93c0e3c0c020460350b
‏‏‏‏Matan‏ ‏‏‏‏ ‏‏ ‏ <drms12@gmail.com>
2022-04-06 15:07:43 +0300

    4.5 - leap year

---
+++ 4.5 - leap year.js
@@ -0,0 +1,13 @@
+function leapYear(year) {
+  if (
+    year % 4 == 0 &&
+    (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0))
+  ) {
+    console.log("It is indeed a leap year.");
+  } else {
+    console.log("This is not a leap year.");
+  }
+}
+leapYear(2012);
+leapYear(2100);
+leapYear(2400);
